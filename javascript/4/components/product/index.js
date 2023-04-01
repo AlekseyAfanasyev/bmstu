a01d2ce6f9d10d0f1e21c7c562519778f16cbdea
Djivs <icemcpe099@gmail.com>
2023-04-01 14:58:04 +0300

    start lab 4

---
+++ javascript/4/components/product/index.js
@@ -0,0 +1,34 @@
+export class ProductComponent {
+    constructor(parent) {
+        this.parent = parent
+    }
+
+    getHTML(data) {
+        return (
+            `
+                <div class="card mb-3" style="width: 540px;">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="${data.src}" class="img-fluid" alt="картинка">
+                        </div>
+                        <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                            <strong>sushi cat</strong> sushi cat
+                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Закрыть"></button>
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">${data.title}</h5>
+                                <p class="card-text">${data.text}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            `
+        )
+    }
+
+    render(data) {
+        const html = this.getHTML(data)
+        this.parent.insertAdjacentHTML('beforeend', html)
+    }
+}
