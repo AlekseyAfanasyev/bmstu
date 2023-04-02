c84e9a7712bc584f1c85a983aad343703a613519
Djivs <icemcpe099@gmail.com>
2023-04-02 23:25:32 +0300

    electronics & java

--- javascript/4/pages/main/index.js
+++ javascript/4/pages/main/index.js
@@ -1,5 +1,10 @@
 import {ProductCardComponent} from "../../components/product-card/index.js";
 import {ProductPage} from "../product/index.js";
+
+import {ajax} from "../../modules/ajax.js";
+import {urls} from "../../modules/urls.js";
+import {groupId} from "../../modules/consts.js";
+
 export class MainPage {
     constructor(parent) {
         this.parent = parent;
@@ -9,45 +14,37 @@ export class MainPage {
         return document.getElementById('main-page')
     }
 
-    getHTML() {
+    getHTML(data) {
         return (
             `
-                <div id="main-page" class="d-flex flex-wrap"><div/>
+                <div class="card" style="width: 300px;">
+                    <img class="card-img-top" src="${data.photo_400_orig}" alt="картинка">
+                    <div class="card-body">
+                        <h5 class="card-title">${data.first_name} ${data.last_name}</h5>
+                        <button class="btn btn-primary" id="click-card-${data.id}" data-id="${data.id}">Нажми на меня</button>
+                    </div>
+                </div>
             `
         )
     }
 
     getData() {
-        return [
-            {
-                id: 1,
-                src: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/cute-cat-photos-1593441022.jpg",
-                title: "Cute kitten",
-                text: "You have never seen a kitten like this before! 1"
-            },
-            {
-                id: 2,
-                src: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/cute-photos-of-cats-curled-up-sleeping-1593184773.jpg",
-                title: "Cute kitten",
-                text: "You have never seen a kitten like this before! 2"
-            },
-            {
-                id: 3,
-                src: "https://i.guim.co.uk/img/media/43352be36da0eb156e8551d775a57fadba8ae6d7/0_0_1440_864/master/1440.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=184376f73721b565014f1d24e5bf645c",
-                title: "Cute kitten",
-                text: "You have never seen a kitten like this before! 3"
-            },
-        ]
+        ajax.post(urls.getGroupMembers(groupId), (data) => {
+            this.renderData(data.response.items)
+        })
     }
 
     clickCard(e) {
-        if (e.target.id === `test-card-${e.target.dataset.id}`) {
-            const img = document.querySelector(`#picture-${e.target.dataset.id}`)
-        } else {
-            const cardId = e.target.dataset.id 
-            const productPage = new ProductPage(this.parent, cardId)
-            productPage.render()
-        }
+        const cardId = e.target.dataset.id 
+        const productPage = new ProductPage(this.parent, cardId)
+        productPage.render()
+    }
+
+    renderData(items) {
+        items.forEach((item) => {
+            const productCard = new ProductCardComponent(this.pageRoot)
+            productCard.render(item, this.clickCard.bind(this))
+        })
     }
     
         
@@ -55,11 +52,7 @@ export class MainPage {
         this.parent.innerHTML = ''
         const html = this.getHTML()
         this.parent.insertAdjacentHTML('beforeend', html)
-        
-        const data = this.getData()
-        data.forEach((item) => {
-            const productCard = new ProductCardComponent(this.pageRoot)
-            productCard.render(item, this.clickCard.bind(this))
-        })
+    
+        this.getData()
     }
 }
