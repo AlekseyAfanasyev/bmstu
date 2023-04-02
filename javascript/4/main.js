278a7c9da8869749e41a141806223217cac8a7f5
Djivs <icemcpe099@gmail.com>
2023-04-02 23:25:32 +0300

    electronics & java

--- javascript/4/main.js
+++ javascript/4/main.js
@@ -1,4 +1,8 @@
 import {MainPage} from "./pages/main/index.js";
+import {urls} from "./urls.js";
+
+console.log(urls.getGroupMembers())
+console.log(1)
 
 const root = document.getElementById('root');
 
